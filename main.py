c64bd613d86f75f6dcd98a30c4c5a35e7d7ede3b
Samir Salman <33979978+samirsalman@users.noreply.github.com>
2020-11-26 16:27:49 +0100

    according_rules file generated

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import json
 import os
 
 
+according = []
+
 def read_files(path1, path2):
     f1 = open(path1, "r+")
     f2 = open(path2, "r+")
@@ -21,13 +23,17 @@ def extract_beliefs(d1, d2):
     return [beliefs1, beliefs2]
 
 
-def rules_equal(r1, r2, rule_type):
+def rules_equal(r1, r2, rule_type, dialog):
+    j = 0
     for k in r1.keys():
         if r1[k] != r2[k]:
             print(f'Rule type: {rule_type}\nKey: {k}\nR1: {r1[k]}\nR2: {r2[k]}')
             print(25 * '*' + "\n")
 
             return False
+        else:
+            according.append({"dialog":dialog, "type":  rule_type, "rule_index" : j})
+        j+=1
     return True
 
 
@@ -45,9 +51,9 @@ def compute_rules(base_path1, base_path2, files1, files2):
 
         for i in range(len(beliefs[0])):
             tot += 1
-            if rules_equal(beliefs[0][i], beliefs[1][i], "belief"):
+            if rules_equal(beliefs[0][i], beliefs[1][i], "belief", files1[file_i].split(".")[0]):
                 b += 1
-            if rules_equal(actions[0][i], actions[1][i], "actions"):
+            if rules_equal(actions[0][i], actions[1][i], "actions",files1[file_i].split(".")[0]):
                 a += 1
 
     print(f'actions: {a}\nbeliefs: {b}\ntot: {tot}')
@@ -80,3 +86,5 @@ medium = open("medium_results.csv", "w+")
 medium.write("action_agreement,belief_agreement\n"
             f"{ac},{be}")
 medium.close()
+according_f = open("according.json", "a")
+json.dump(according, according_f)
